6f42dd3dbabe93df27b5e3bb02d5e332bfa215de
szholbarys <sakenzholbarys@gmail.com>
2024-10-01 15:03:28 +0500

    Initial Commit

---
+++ IAmRich/IAmRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichApp.swift
+//  IAmRich
+//
+//  Created by Zholbarys on 01.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
